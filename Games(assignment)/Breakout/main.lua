d9627c837411a63ced2bb4ee8575c4db3758d25c
Saptarshisaha777 <saptarshisaha777@gmail.com>
2020-05-01 22:58:36 +0530

    added the StartState and other importsnt files

---
+++ Games(assignment)/Breakout/main.lua
@@ -0,0 +1,132 @@
+require 'src/Dependencies'
+
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  -- app window title
+  love.window.setTitle('Breakout')
+
+  math.randomseed(os.time())
+
+  gFonts = {
+    ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+    ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+    ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+  }
+  love.graphics.setFont(gFonts['small'])
+
+
+
+  gTextures = {
+    ['background'] = love.graphics.newImage('graphics/background.png'),
+    ['main'] = love.graphics.newImage('graphics/breakout.png'),
+    ['arrows'] = love.graphics.newImage('graphics/arrows.png'),
+    ['hearts'] = love.graphics.newImage('graphics/hearts.png'),
+    ['particle'] = love.graphics.newImage('graphics/particle.png')
+  }
+
+
+
+  gSounds = {
+    ['paddle-hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'stream'),
+    ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+    ['wall-hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static'),
+    ['confirm'] = love.audio.newSource('sounds/confirm.wav', 'static'),
+    ['select'] = love.audio.newSource('sounds/select.wav', 'static'),
+    ['no-select'] = love.audio.newSource('sounds/no-select.wav', 'static'),
+    ['brick-hit-1'] = love.audio.newSource('sounds/brick-hit-1.wav', 'static'),
+    ['brick-hit-2'] = love.audio.newSource('sounds/brick-hit-2.wav', 'static'),
+    ['hurt'] = love.audio.newSource('sounds/hurt.wav', 'static'),
+    ['victory'] = love.audio.newSource('sounds/victory.wav', 'static'),
+    ['recover'] = love.audio.newSource('sounds/recover.wav', 'static'),
+    ['high-score'] = love.audio.newSource('sounds/high_score.wav', 'static'),
+    ['pause'] = love.audio.newSource('sounds/pause.wav', 'static'),
+
+    ['music'] = love.audio.newSource('sounds/music.wav', 'stream')
+  }
+
+  gSounds['music']:setLooping(true)
+
+  gStateMachine = StateMachine {
+    ['start'] = function() return StartState() end,
+  }
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = true,
+    vsync = true
+  })
+
+  gStateMachine:change('start')
+
+  love.keyboard.keysPressed = {}
+
+end
+
+
+function love.resize(w, h)
+  push:resize(w, h)
+end
+
+
+function love.keypressed(key)
+  -- body...
+  love.keyboard.keysPressed[key] = true
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+
+-- it is needed to check if a specific button is pressed by user by other classes as table values cannot be called directly
+function love.keyboard.wasPressed(key)
+
+  if love.keyboard.keysPressed[key] then
+    return true
+  else
+    return false
+  end
+
+end
+
+
+function love.update(dt)
+
+  gStateMachine:update(dt)
+
+  love.keyboard.keysPressed = {}
+
+end
+
+
+function love.draw()
+
+  push:start()
+
+  local backgroundWidth = gTextures['background']:getWidth()
+  local backgroundHeight = gTextures['background']:getHeight()
+
+  love.graphics.draw(gTextures['background'],
+    -- starting from 0 0 coordinate
+    0, 0,
+    -- no rotation
+    0,
+    -- scaling the image's x & y axis to fill the screen
+    VIRTUAL_WIDTH / (backgroundWidth - 1), VIRTUAL_HEIGHT / (backgroundHeight - 1)
+  )
+
+  displayFPS()
+
+  gStateMachine:render()
+
+  push:finish()
+
+end
+
+
+function displayFPS()
+  -- simple FPS display across all states
+  love.graphics.setFont(gFonts['small'])
+  love.graphics.setColor(0, 255, 0, 255)
+  love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 5, 5)
+  love.graphics.setColor(255, 255, 255, 255)
+end
---
+++ Games(assignment)/Breakout/src/states/StartState.lua
@@ -0,0 +1,56 @@
+StartState = Class{__includes = BaseState}
+
+local option_no = 0
+local total_option = 2
+
+function StartState:update(dt)
+
+
+  if love.keyboard.wasPressed('up') then
+    option_no = (option_no - 1) % total_option
+    --option_no = option_no < 0 and -option_no or option_no
+    gSounds['paddle-hit']:play()
+  elseif love.keyboard.wasPressed('down') then
+    option_no = (option_no + 1) % total_option
+    gSounds['paddle-hit']:play()
+  end
+
+  --[[if love.keyboard.wasPressed('up') or love.keyboard.wasPressed('down') then
+      option_no = option_no == 1 and 2 or 1
+      gSounds['paddle-hit']:play()
+  end]]
+
+end
+
+function StartState:render(dt)
+
+  love.graphics.setFont(gFonts['large'])
+  love.graphics.printf("BREAKOUT", 0, VIRTUAL_HEIGHT / 3,
+      VIRTUAL_WIDTH, 'center')
+
+  -- instructions
+  love.graphics.setFont(gFonts['medium'])
+
+  -- if we're highlighting 1, render that option blue
+  if option_no == 0 then
+      love.graphics.setColor(0, 255, 255, 255)
+  end
+  love.graphics.printf("START", 0, VIRTUAL_HEIGHT / 2 + 70,
+      VIRTUAL_WIDTH, 'center')
+
+  -- reset the color
+  love.graphics.setColor(255, 255, 255, 255)
+
+  -- render option 2 blue if we're highlighting that one
+  if option_no == 1 then
+      love.graphics.setColor(0, 255, 255, 255)
+  end
+  love.graphics.printf("HIGH SCORES", 0, VIRTUAL_HEIGHT / 2 + 90,
+      VIRTUAL_WIDTH, 'center')
+
+  -- reset the color
+  love.graphics.setColor(255, 255, 255, 255)
+
+
+
+end
